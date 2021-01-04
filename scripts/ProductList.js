1547b5b6d04539291ffbc2e35ea54ab7e968e8cf
pazia008 <pazia008@gmail.com>
2021-01-04 14:52:03 -0600

    new repo

---
+++ scripts/ProductList.js
@@ -0,0 +1,30 @@
+import { useProducts } from "./ProductProvider.js"
+import { useProductTypes } from "./ProductTypeProvider.js"
+import { Product } from "./Product.js"
+
+const contentTarget = document.querySelector(".products")
+
+export const ProductList = () => {
+    // Get a reference to both arrays
+    const products = useProducts()
+    const productTypes = useProductTypes()
+
+    // Iterate the array of products with .map()
+    const arrayOfProductHTMLRepresentations = products.map(product => {
+
+        /*
+            Find the type for this product by using the .find()
+            method on the product type array
+        */
+        const type = productTypes.find(type => type.id === product.productTypeId)
+
+        // Build HTML representation of a product
+        const html = Product(product, type)
+
+        return html
+    })
+
+    const stringOfAllRepresentations = arrayOfProductHTMLRepresentations.join("")
+
+    contentTarget.innerHTML = stringOfAllRepresentations
+}
